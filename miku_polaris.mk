b34bb54f23b9528c3b56e32095f822dff5ca3ac2
Canary233 <2096276316@qq.com>
2022-02-04 02:59:01 +0800

    polaris:miku-ify

--- miku_polaris.mk
+++ miku_polaris.mk
@@ -6,22 +6,10 @@
 
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
-# Inherit some common PixelExperience stuff.
-EVO_MAINTAINER := jjpprrrr
-EVO_SUPPORT_URL := https://t.me/EvolutionXPolaris
-EVO_DONATE_URL := https://paypal.me/jjpprrrr
-WITH_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_SUPPORTS_QUICK_TAP := true
-DEVICE_REQUIRES_CARRIER_APPS := true
-
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_polaris
+PRODUCT_NAME := miku_polaris
 PRODUCT_DEVICE := polaris
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
